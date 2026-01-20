c4637aceac300d074b6e76b5496d9137beaf2c46
Mack2001-cloud <rugeriomaldonado@gmail.com>
2026-01-20 03:59:42 -0600

    Log out when returning to login

--- auth/login.php
+++ auth/login.php
@@ -1,4 +1,13 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) session_start();
+if (isset($_SESSION["id_usuario"])) {
+  $_SESSION = [];
+  if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), "", time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+  }
+  session_destroy();
+}
 require_once __DIR__ . "/../config/app.php";
 $err = (int)($_GET["error"] ?? 0);
 
